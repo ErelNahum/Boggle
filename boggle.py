39dce9c23895667c82a900e19fa093b678f4e1b0
ErelNahum <erelnahum2003@gmail.com>
2022-01-09 15:11:22 +0200

    hhh

--- boggle.py
+++ boggle.py
@@ -1,5 +1,12 @@
+import sys
+
 from gui import GUI
 from boggle_board_randomizer import randomize_board
-if __name__ == '__main__':
+
+def play_again():
     my_game = GUI(randomize_board())
+    my_game.run()
+
+if __name__ == '__main__':
+    my_game = GUI(randomize_board(), play_again, sys.exit)
     my_game.run()
